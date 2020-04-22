d6c70201250800b37326f9e9ceecb950843ba232
hanifyudhaa <hanifyudhaa@gmail.com>
2020-04-22 16:55:36 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,313 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Crete+Round|Grand+Hotel|Montez&display=swap"
+          rel="stylesheet">
+    <link rel="icon" href="images/love.ico" type="image/gif" sizes="16x16">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <title>Wedding of Apang and Nifo</title>
+</head>
+<body>
+
+<header style=""></header>
+
+<section class="banner container" data-aos="fade-up">
+    <div class="mb-5 mt-5">
+        <h1 class="text-center the-wedding">THE WEDDING</h1>
+        <div class="row">
+            <div class="col" style=" padding: 0; text-align: right;">
+                <img class=" tag-timer
+            " src="images/tag.png" alt="" style="max-width: 80%">
+            </div>
+            <div class="col" style=" padding: 0; text-align: left;">
+                <img class="tag-timer" src="images/tag.png" alt=""
+                     style="max-width: 80%; transform: rotateY(180deg)">
+            </div>
+        </div>
+    </div>
+
+    <div class="row tanggal">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-2 text-right">
+            <h2>Minggu, <br> 22</h2>
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-8 text-center">
+            <img src="images/foto2.png" alt="" style="max-width: 100%;">
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-2 text-left">
+            <h2>Maret, <br> 2020</h2>
+        </div>
+    </div>
+
+    <div class="pesan text-center">
+        <hr style="border: solid white 1px">
+        <h1 class="pesan">Dengan memohon ridho Allah Subhanahu Wa Ta'ala, kami mengharapkan kehadiran
+            Bapak/Ibu/Saudara/i pada acara
+            pernikahan kami :</h1>
+    </div>
+</section>
+
+<section class="pengantin container text-center" data-aos="fade-up">
+    <div class="row">
+        <div class="col-md-12 pria" style="max-width: 100%" data-aos="fade-right">
+            <div class="row">
+                <div class="col">
+                    <img class="foto" src="images/cowok.png" alt="" style="">
+                </div>
+                <div class="col">
+                    <p class="mempelai">Mempelai Pria</p>
+                    <p class="nama">APANG RINALDO</p>
+                    <p class="putra">
+                        <b>Putra dari</b>
+                        <br>
+                        Bapak Saprudin & Ibu Atni Mardiani
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 wanita mt-2" style="max-width: 100%" data-aos="fade-left">
+            <div class="row">
+                <div class="col">
+                    <p class="mempelai">Mempelai Wanita</p>
+                    <p class="nama">NIFO WINONA AL GASYIYA</p>
+                    <p class="putra">
+                        <b>Putri dari</b>
+                        <br>
+                        Bapak Zainuri & Ibu Siti Suryani
+                    </p>
+                </div>
+                <div class="col">
+                    <img class="foto" src="images/cewek.png" alt="" style="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pesan container text-center">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col" data-aos="zoom-in">
+                    <img class="tag-timer" src="images/tag.png" alt="" style="max-width: 50%">
+                </div>
+                <div class="col" data-aos="zoom-in">
+                    <img class="tag-timer" src="images/tag.png" alt=""
+                         style="max-width: 50%; transform: rotateY(180deg)">
+                </div>
+            </div>
+            <p class="kata-kata" data-aos="zoom-in">“Semoga Allah menghimpun yang terserak dari
+                keduanya, memberkati mereka berdua dan kiranya Allah meningkatkan kualitas keturunan mereka,
+                menjadikannya pembuka pintu rahmat, sumber ilmu dan hikmah serta pemberi rasa aman bagi
+                umat.”<br><br><b>(Doa Nabi Muhammad SAW, pada pernikahan putrinya<br>Fatimah Azzahra dengan Ali Bin Abi
+                    Thalib)</b>
+            </p>
+        </div>
+    </div>
+</section>
+
+<section class="akad container text-center" data-aos="flip-down">
+    <div class="row">
+        <div class="col">
+            <div class="akad-bg"></div>
+        </div>
+        <div class="col text-center akad-bg2">
+            <img src="images/akad.png" alt="" style="max-width: 70%">
+            <p class="pesan">
+                <b>Lokasi : </b>
+                <br>
+                Gedung Serbaguna SDN 2 Pohgading, Pringgabaya
+            </p>
+
+            <div class="row mb-3">
+                <div class="col">
+                    <i class="fa fa-calendar icon-akad" aria-hidden="true"></i>
+                    <h2 class="mt-3">Minggu,<br> 22 Maret 2020</h2>
+                </div>
+                <div class="col">
+                    <i class="fa fa-clock-o icon-akad" aria-hidden="true"></i>
+                    <h2 class="mt-3">08.30 - 10.00 <br> WITA</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="resepsi container text-center" data-aos="flip-down">
+    <div class="row">
+        <div class="col text-center resepsi-bg2">
+            <img src="images/resepsi.png" alt="" style="max-width: 70%">
+            <p class="pesan">
+                <b>Lokasi : </b>
+                <br>
+                Gedung Serbaguna SDN 2 Pohgading, Pringgabaya
+            </p>
+
+            <div class="row mb-3">
+                <div class="col">
+                    <i class="fa fa-calendar icon-resepsi" aria-hidden="true"></i>
+                    <h2 class="mt-3">Minggu,<br> 22 Maret 2020</h2>
+                </div>
+                <div class="col">
+                    <i class="fa fa-clock-o icon-resepsi" aria-hidden="true"></i>
+                    <h2 class="mt-3">10.30 - 12.30 <br> WITA</h2>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="resepsi-bg"></div>
+        </div>
+    </div>
+</section>
+
+<section class="counter container text-center mt-5 p-5" data-aos="zoom-in-up">
+    <h1 class="text-center mb-5">"Jazakumullahu Khair"</h1>
+
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center">
+            <h2>Countdown Timer</h2>
+            <div class="row">
+                <div class="col">
+                    <img class="tag-timer" src="images/tag.png" alt="" style="max-width: 80%">
+                </div>
+                <div class="col">
+                    <img class="tag-timer" src="images/tag.png" alt=""
+                         style="max-width: 80%; transform: rotateY(180deg)">
+                </div>
+            </div>
+
+            <div class="row mt-3">
+                <div class="col text-center">
+                    <p id="hari" class="counter-isi"></p>
+                </div>
+                <div class="col text-center">
+                    <p id="jam" class="counter-isi"></p>
+                </div>
+                <div class="col text-center">
+                    <p id="menit" class="counter-isi"></p>
+                </div>
+                <div class="col text-center">
+                    <p id="detik" class="counter-isi"></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <p class="counter-label">Hari</p>
+                </div>
+                <div class="col text-center">
+                    <p class="counter-label">Jam</p>
+                </div>
+                <div class="col text-center">
+                    <p class="counter-label">Menit</p>
+                </div>
+                <div class="col text-center">
+                    <p class="counter-label">Detik</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+            <h2>Pesan Untuk Mempelai</h2>
+
+            <div class="row">
+                <div class="col">
+                    <img class="tag-timer" src="images/tag.png" alt="" style="max-width: 80%">
+                </div>
+                <div class="col">
+                    <img class="tag-timer" src="images/tag.png" alt=""
+                         style="max-width: 80%; transform: rotateY(180deg)">
+                </div>
+            </div>
+            <a href="https://forms.gle/9MRULsPRA8bmWTqDA" class="btn btn-light google-form">Klik Disini</a>
+        </div>
+    </div>
+</section>
+
+<div class="bg-counter"></div>
+
+<audio controls autoplay>
+    <source src="images/song.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+</audio>
+
+<section>
+    <div class="copyright text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright-push-right center-copyright">
+                        <img src="images/logo.png" alt="">
+                        <h3>Copyright © 2019 LiburAja. All Rights Reserved.</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="petunjuk text-center" id="lokasi">
+    <a href="https://maps.app.goo.gl/y51V4pyQeto9mmhT7" class="text-center">
+        <img src="images/maps.png" alt="" style="max-width: 60%">
+    </a>
+    <h2>Petunjuk <br> Arah</h2>
+</div>
+
+<script>
+    window.onload = function() {
+        var context = new AudioContext();
+    }
+</script>
+
+<script>
+    // Set the date we're counting down to
+    var countDownDate = new Date("March 22, 2020 09:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        // document.getElementById("demo").innerHTML = days + " hari " + hours + " jam "
+        //     + minutes + " menit " + seconds + " detik ";
+
+        document.getElementById("hari").innerHTML = days;
+        document.getElementById("jam").innerHTML = hours;
+        document.getElementById("menit").innerHTML = minutes;
+        document.getElementById("detik").innerHTML = seconds;
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+</script>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="css/aos.js"></script>
+<script>
+    AOS.init({
+        duration: 1500,
+    });
+</script>
+</body>
+</html>
